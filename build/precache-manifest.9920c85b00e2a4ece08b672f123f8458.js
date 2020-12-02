39eae4fd81848c81ee7b96fa1969a174b89970dc
Fajar AM <somabangsa@gmail.com>
2020-12-02 09:57:59 +0700

    update links

---
+++ build/precache-manifest.9920c85b00e2a4ece08b672f123f8458.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7a18999122c796903e98",
+    "url": "/static/js/main.7a189991.chunk.js"
+  },
+  {
+    "revision": "faea0f0eec0e13fbd31d",
+    "url": "/static/js/1.faea0f0e.chunk.js"
+  },
+  {
+    "revision": "7a18999122c796903e98",
+    "url": "/static/css/main.253f64db.chunk.css"
+  },
+  {
+    "revision": "d76f2f821d22f5fb4fe8dd3519222836",
+    "url": "/index.html"
+  }
+];
